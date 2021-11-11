c686a96194655b9a1ea254c6c83f90657379fc6e
Drew Peterson <drew.a.peterson@hotmail.com>
2021-11-10 17:41:35 -0800

    modify menu to not push main content back and forth

--- js/scripts.js
+++ js/scripts.js
@@ -31,10 +31,10 @@ contact.addEventListener("click", () => { toggleView("myContact"); }, false);
 
 function openNav() {
   document.getElementById("myNavbar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  // document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("myNavbar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  // document.getElementById("main").style.marginLeft = "0";
 }
